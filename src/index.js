e89cdeaa16ed6ccce5f5f6dacde54709b6cd2f4e
denalibalser <denali.balser167@gmail.com>
2020-11-24 10:10:15 -0700

    commit tree 7f59ec044b152d9e374cae4c282e86b24af06ce2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function fetchImg() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(response => response.json())
+    .then(json => renderImg(json));
+    
+  }
+
+  function renderImg(json) {
+      const div = document.querySelector('#dog-image-container')
+      json.forEach(img => {
+          const li = document.createElement('li')
+          li.innerHTML =  `<li>${img}</li>`
+          div.appendChild(li)
+      })
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchImg();
+});
+
